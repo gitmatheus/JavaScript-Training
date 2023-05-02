f9e64a14e8e737004bb2a457e3a8e995da53b55a
Matheus Goncalves <matheus.goncalves@salesforce.com>
2023-05-02 09:25:37 -0700

    feat: moving comments

--- 02-closures/memoization.js
+++ 02-closures/memoization.js
@@ -1,34 +1,3 @@
-// var isPrime = (function(){
-//     var results = {}
-//     function isPrime(no) {
-//         for (var i = 2; i <= (no / 2); i++) {
-//             if (no % i === 0) {
-//                 return false
-//             }
-//         }
-//         return true
-//     }
-//     return function(no){
-//         if (results.hasOwnProperty(no)) return results[no]
-//         console.log('processing :', no)
-//         results[no] = isPrime(no)
-//         return results[no]
-//     }
-// })()
-
-// var isEven = (function () {
-//     var results = {}
-//     function isEven(no) {
-//         return no % 2 === 0;
-//     }
-//     return function (no) {
-//         if (results.hasOwnProperty(no)) return results[no]
-//         console.log('processing :', no)
-//         results[no] = isEven(no)
-//         return results[no]
-//     }
-// })()
-
 // Modify the memoize function in such a a way that it can memoize the result of
 // any function with any number of arguments
 function memoize(fn) {
@@ -65,3 +34,36 @@ var add = memoize(function add(args) {
     ? parseArg(arguments[0])
     : parseArg(arguments[0]) + add.apply(this, [].slice.call(arguments, 1));
 });
+
+// https://github.com/tkmagesh/Salesforce-AdvJs-May-2023/blob/main/02-closures/memoization.js
+
+// var isPrime = (function(){
+//     var results = {}
+//     function isPrime(no) {
+//         for (var i = 2; i <= (no / 2); i++) {
+//             if (no % i === 0) {
+//                 return false
+//             }
+//         }
+//         return true
+//     }
+//     return function(no){
+//         if (results.hasOwnProperty(no)) return results[no]
+//         console.log('processing :', no)
+//         results[no] = isPrime(no)
+//         return results[no]
+//     }
+// })()
+
+// var isEven = (function () {
+//     var results = {}
+//     function isEven(no) {
+//         return no % 2 === 0;
+//     }
+//     return function (no) {
+//         if (results.hasOwnProperty(no)) return results[no]
+//         console.log('processing :', no)
+//         results[no] = isEven(no)
+//         return results[no]
+//     }
+// })()
